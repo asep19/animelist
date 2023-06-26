f9cb95e3a66bd11841c40c734e2d28f5c230c728
asep <aseprendi487@gmail.com>
2023-06-26 11:09:22 +0700

    fetching data done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import '@/styles/globals.css'
+// import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  //v.1
+  // return <Component {...pageProps} />;
+
+  //v.2 --- ini settingan kalo semuanya mau layout yang sama
+  // return (
+  //   <Layout>
+  //     <Component {...pageProps} />
+  //   </Layout>
+  // );
+
+  //v.3 ---- settingan kalo mau page tertentu
+  const getLayout = Component.getLayout || ((page) => page);
+  return getLayout(<Component {...pageProps} />);
 }
