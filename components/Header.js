bd0f68f391cb486f7e55ff8333b7aad475975d83
asep <aseprendi487@gmail.com>
2023-06-29 21:44:07 +0700

    Implement Search Functionality

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,24 @@
+import Search from "./Search"
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <>
+      <header className="bg-gray-50">
+        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+          <div className="flex items-center sm:justify-between sm:gap-4"></div>
+          {/* <Search /> */}
+          <div className="mt-8">
+            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+              Welcome Back, Asep!
+            </h1>
+
+            <p className="mt-1.5 text-sm text-gray-500">
+              Your website has seen a 52% increase in traffic in the last month. Keep
+              it up! 🚀
+            </p>
+          </div>
+        </div>
+      </header>
+
+    </>
+  )
 }
