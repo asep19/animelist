91d5a52d537eda5c5cffbb0338ce515cd886107d
asep <aseprendi487@gmail.com>
2024-01-03 19:32:10 +0700

    Add: <TopAnime /> component

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import CurrentSeasons from "@/components/current-season"
+import TopAnime from "@/components/top-anime"
 
 export default function Page() {
   
   return (
-    <section className="container mx-auto">
+    <section className="container mx-auto space-y-8">
+      <TopAnime />
       <CurrentSeasons />
     </section>
   )
---
+++ components/top-anime.jsx
@@ -0,0 +1,19 @@
+import { fetchAPI } from "@/utils/fetchApi"
+import AnimeCard from "./anime-card"
+
+export default async function TopAnime() {
+  const response = await fetchAPI('/top/anime?filter=favorite&limit=10&sfw=true')
+  const animes = response.data
+
+  console.log(response)
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">Top Anime</h1>
+      <div className="flex flex-wrap items-start mt-4">
+        {animes.map((topAnime) => (
+          <AnimeCard {...topAnime}/>
+        ))}
+      </div>
+    </div>
+  )
+}
