800a35a4ddce0b603e6bdafe2e501c49b4033d7c
asep <aseprendi487@gmail.com>
2024-01-05 09:04:27 +0700

    Add: pagination in genre page

--- app/genres/[id]/page.jsx
+++ app/genres/[id]/page.jsx
@@ -1,15 +1,32 @@
 import { fetchAPI } from "@/utils/fetchApi"
 import AnimeCard from "@/components/anime-card"
+import { 
+  Pagination, PaginationContent, PaginationItem, PaginationNext, PaginationPrevious
+} from "@/components/ui/pagination"
 
-export default async function Page({ params }) {
-  const response = await fetchAPI(`/anime?genres=${params.id}&limit=20&order_by=score&sort=desc`)
+export default async function Page({ params, searchParams }) {
+  const page = searchParams['page'] ?? '1'
+  
+  const response = await fetchAPI(`/anime?genres=${params.id}&page=${page}&order_by=score&sort=desc`)
   const animes = response.data
+  const paginationCount = response.pagination.last_visible_page 
+
   return (
       <div className="flex flex-wrap items-start">
 
       {animes.map((anime) => (
         <AnimeCard {...anime}/>
       ))}
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href={`/genres/${params.id}?page=${Number(page) - 1}`} />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href={`/genres/${params.id}?page=${Number(page) + 1}`} />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   )
 }
